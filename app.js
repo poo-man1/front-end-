08fe9fa83f68d8325219257fcdb3163c2c1b245a
POO <poomani8807@gmail.com>
2025-12-30 21:12:30 -0800

    Update app.js

--- app.js
+++ app.js
@@ -101,3 +101,12 @@ function reset() {
   socket.disconnect();
   setTimeout(() => location.reload(), 500);
 }
+iceServers: [
+  { urls: "stun:stun.l.google.com:19302" },
+  {
+    urls: "turn:openrelay.metered.ca:80",
+    username: "openrelayproject",
+    credential: "openrelayproject"
+  }
+]
+
